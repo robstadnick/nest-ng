e019d4a84b5cfe419f3d0e2ac7d7f003947fe75b
Rob Stadnick <robstadnick@gmail.com>
2019-08-04 13:49:51 -0600

    auth controlers set up

--- server/app.module.ts
+++ server/app.module.ts
@@ -5,6 +5,17 @@ import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './modules/users/user.module';
 
+const domino = require('domino');
+const win = domino.createWindow();
+
+global['window'] = win;
+global['document'] = win.document;
+global['navigator'] = win.navigator;
+global['CSS'] = undefined;
+global['Event'] = undefined;
+global['localStorage'] = undefined;
+global['getItem'] = undefined;
+
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
--- server/auth/middleware/auth.middleware.ts
+++ server/auth/middleware/auth.middleware.ts
@@ -1,7 +1,7 @@
 import * as jwt from 'jsonwebtoken';
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { MessageCodeError } from '../../errors';
-// import { ModelUser } from '../../database/models/users/user.model'
+import { ModelUser } from '../../database/models/users/user.model'
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
@@ -9,13 +9,13 @@ export class AuthMiddleware implements NestMiddleware {
         if (req.headers.authorization && (req.headers.authorization as string)) {
             const token = (req.headers.authorization as string).replace('Bearer access-token-', '')
             const decoded: any = jwt.verify(token, process.env.JWT_KEY || '');
-            // const user = await ModelUser.findOne<ModelUser>({
-            //     where: {
-            //         id: decoded.id,
-            //         email: decoded.email
-            //     }
-            // });
-            // if (!user) throw new MessageCodeError('request:unauthorized');
+            const user = await ModelUser.findOne<ModelUser>({
+                where: {
+                    id: decoded.id,
+                    email: decoded.email
+                }
+            });
+            if (!user) throw new MessageCodeError('request:unauthorized');
             next();
         } else {
             throw new MessageCodeError('request:unauthorized');
